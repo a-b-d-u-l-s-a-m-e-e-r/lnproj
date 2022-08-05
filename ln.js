57b79ffd25b95e6e3b985767c940ede0bf7e65d9
Sameer <sheikhsameercr7@gmail.com>
2022-08-06 00:26:37 +0530

    first commit

---
+++ ln.js
@@ -0,0 +1,23 @@
+function show() {
+   gsap.registerPlugin(ScrollTrigger);
+   const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#bg"),
+      smooth: true
+   });
+   locoScroll.on("scroll", ScrollTrigger.update);
+   ScrollTrigger.scrollerProxy("#bg", {
+      scrollTop(value) {
+         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+         return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      },
+      pinType: document.querySelector("#bg").style.transform ? "transform" : "fixed"
+   });
+   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+   ScrollTrigger.refresh();
+}
+
+show();
+
+
